57eb301306ee1c71419c772fe44d215cdac1ef1a
Muaaz Farooq Khan <kmuaaz18@gmail.com>
2025-08-27 22:32:26 +0500

    use state and check how to call component in side the component and what is different ways to call them

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,103 +1,56 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState('Muaaz');
+  const [outer, setOuter] = useState(0);
+  let nameMe= 'Texting Me'
+  const clickMe=()=>{
+    nameMe="Don't Texting Me"
+    setName("Muaaz Farooq Khan")
+  }
+  const NewComponent= ()=> {
+    debugger
+    const [name, setName] = useState('Muaaz');
+    const clickMe=()=>{
+      nameMe="Don't Texting Me"
+      setName("Muaaz Farooq Khan")
+    }
+    return(
+      <>
+            <h1>This is inner Component</h1>
+            <h1>Hello World {name}</h1>
+      <h2>{nameMe}</h2>
+      <button onClick={()=> clickMe()}>Click me to change the messages of the inner component</button>
+            </>
+      
+    )
+  }
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      <h1>Hello World {name}</h1>
+      <h2>{nameMe}</h2>
+      <button onClick={() => setOuter(outer + 1)}>Increment Outer</button>
+      <button onClick={()=> clickMe()}>Click me to change the message</button>
+      {/* React component instance (with state, lifecycle, hooks, reconciliation). */}
+      {/* The correct way to render a React component.
+          React treats it as a component, not just a function.
+          It creates its own lifecycle, props, state, hooks, etc. */}
+      {/* <NewComponent/>
+      NewComponent() */}
+      <h2>❌ Function Call (will break)</h2>
+      {outer % 2 === 0 && NewComponent()}
+      <h2>❌ Function Call (will not break)</h2>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      {outer % 2 === 0 && <NewComponent/>}
+
+      {/* plain function call, React doesn’t treat it as a component. */}
+      {/* React will insert the returned JSX into the parent.
+          But again:
+          No state management
+          No lifecycle
+          No hooks allowed */}
+      {/* {NewComponent()} */}
     </div>
   );
 }
